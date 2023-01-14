023feff02d6f3633ae929c69622cf1c6bc7109fc
Fernanda Dias <fernandadias.dev@gmail.com>
2023-01-14 13:26:35 -0300

    refactor: :recycle: menu da header

---
+++ assets/js/main.js
@@ -0,0 +1,20 @@
+/*=============== SHOW MENU ===============*/
+const navMenu = document.querySelector('#nav-menu'),
+    navToggle = document.querySelector('#nav-toggle'),
+    navClose = document.querySelector('#nav-close')
+
+/*===== MENU SHOW =====*/
+/* Validate if constant exists */
+if (navToggle) {
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu');
+    })
+}
+
+/*===== MENU HIDDEN =====*/
+/* Validate if constant exists */
+if (navClose) {
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu');
+    })
+}
