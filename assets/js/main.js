f45086c3d9878811b8c4bd861e875faff77e310a
Fernanda Dias <fernandadias.dev@gmail.com>
2023-01-20 17:02:05 -0300

    feat: :sparkles: adiciona função de scrollup

--- assets/js/main.js
+++ assets/js/main.js
@@ -37,3 +37,11 @@ const scrollHeader = () => {
         : header.classList.remove('bg-header')
 }
 window.addEventListener('scroll', scrollHeader)
+
+/*=============== SHOW SCROLL UP ===============*/ 
+function scrollUp(){
+    const scrollUp = document.querySelector('#scroll-up');
+    // When the scroll is higher than 400 viewport height, add the show-scroll class to the a tag with the scroll-top class
+    if(this.scrollY >= 400) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
